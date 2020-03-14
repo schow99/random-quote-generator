b242601f5664040a723dfe4c1397fe803cdb5f6c
schow99 <samuelchow93@gmail.com>
2020-03-13 22:08:34 -0400

    Updates

---
+++ precache-manifest.8eccb0ba54e183e073d05ecc59e913cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "355b0d097a93b79950c70fb6cbb363a3",
+    "url": "/random-quote-generator/index.html"
+  },
+  {
+    "revision": "c4e00464fea2b64bd4fe",
+    "url": "/random-quote-generator/static/css/main.8d97af9d.chunk.css"
+  },
+  {
+    "revision": "cacccde92cd9f84c8217",
+    "url": "/random-quote-generator/static/js/2.8a6f635e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/random-quote-generator/static/js/2.8a6f635e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4e00464fea2b64bd4fe",
+    "url": "/random-quote-generator/static/js/main.338b0630.chunk.js"
+  },
+  {
+    "revision": "3d3a8f78c21b62b244e0",
+    "url": "/random-quote-generator/static/js/runtime-main.36be01a7.js"
+  }
+]);
